34d3488637456dfed48a33a32628402dc2f1d686
Ishraq <ishraq.h.c@gmail.com>
2022-02-01 18:48:42 +0100

    basic tictactoe

---
+++ src/components/TicTacToe.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import "./TicTacToe.css";
+
+const TictacToe = () => {
+  const [turn, setTurn] = useState("X");
+  const [cells, setCells] = useState(Array(9).fill(""));
+  const [winner, setWinner] = useState("");
+  const [draw, setDraw] = useState(false);
+
+  const checkForWinner = (squares) => {
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diagonal: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+          squares[pattern[0]] === "" ||
+          squares[pattern[1]] === "" ||
+          squares[pattern[2]] === ""
+        ) {
+        } else if (
+          squares[pattern[0]] === squares[pattern[1]] &&
+          squares[pattern[1]] === squares[pattern[2]]
+        ) {
+          setWinner(squares[pattern[0]]);
+        }
+      });
+    }
+
+    if (!squares.includes("")) {
+      console.log("All cells are filled");
+      setDraw(true);
+    }
+  };
+
+  const handleOnClick = (num) => {
+    if (cells[num] !== "") {
+      alert("Already Clicked: " + num);
+      return;
+    }
+
+    let squares = [...cells];
+
+    if (turn === "X") {
+      squares[num] = "X";
+      setTurn("O");
+    } else {
+      squares[num] = "O";
+      setTurn("X");
+    }
+
+    checkForWinner(squares);
+    setCells(squares);
+  };
+
+  const handleRestart = () => {
+    setWinner(null);
+    setCells(Array(9).fill(""));
+  };
+
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleOnClick(num)}>{cells[num]}</td>;
+  };
+
+  return (
+    <div className="container">
+      <table>
+        <h3>Turn: {turn}</h3>
+        <tbody>
+          <tr>
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
+          </tr>
+          <tr>
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
+          </tr>
+          <tr>
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
+          </tr>
+        </tbody>
+      </table>
+      {winner && (
+        <>
+          <h3>Player {winner} is the Winner</h3>
+          <button onClick={() => handleRestart()}>Play Again</button>
+        </>
+      )}
+      {draw && (
+        <>
+          <h3>This match is a draw!</h3>
+          <button onClick={() => handleRestart()}>Play Again</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TictacToe;
